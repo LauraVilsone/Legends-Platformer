42ea51324bd2c9d8b8304735f22b0feca87bf9a0
Laura Vilsone <laura.vilsone@gmail.com>
2024-05-13 13:36:59 +0300

    Added Unity project

---
+++ Assets/Scripts/PatrolPoints.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolPoints : MonoBehaviour
+{
+    public Transform[] patrolPoints;
+    public int currentPatrolPoint = 0;
+    // Start is called before the first frame update
+    
+    public void Partol()
+    {
+        Vector3 moveToPoint = patrolPoints[currentPatrolPoint].position;
+        transform.position = Vector3.MoveTowards(transform.position, moveToPoint, RefactorEnemy.enemyStats.walkSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, moveToPoint) < 0.01f)
+        {
+            currentPatrolPoint++;
+            if (currentPatrolPoint > patrolPoints.Length - 1)
+            {
+                currentPatrolPoint = 0;
+            }
+        }
+    }
+}
